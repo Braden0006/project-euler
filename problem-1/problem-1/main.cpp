3b1806c718db2f8850601743d21612e67225dce6
Braden0006 <bradenwhitch@gmail.com>
2024-01-02 16:39:45 -0700

    Finished problem 1 functionality, added comments

--- problem-1/problem-1/main.cpp
+++ problem-1/problem-1/main.cpp
@@ -4,5 +4,19 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World" << endl;
+	int num = 1000;
+
+	int total = 0;
+
+	// Loops from 1-1000 and adds all multiples of three and five to "total" variable
+	for (int i = 1; i < num; ++i) {
+		if (((i % 5) == 0) || ((i % 3) ==0)) {
+			cout << i << endl;
+			total += i;
+		}
+	}
+
+	cout << endl;
+	cout << total << endl;
+
 }
